51ee89f15870b54b9b94517298923b57d40efe7f
WangSail <624440896.com>
2018-02-09 16:23:50 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="/css/app.css">
    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
   </head>
-  <body>
-    @yield('content')
-  </body>
+   @include('layouts._header')
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+      
+    </div>
 </html>
