13e84c152c09c14040dfdf137b08bf3c207066a7
WangSail <624440896.com>
2018-02-11 08:57:50 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Models\User;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //头像
+    public function gravatar($size = '100')
+    {
+        $hash=md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
